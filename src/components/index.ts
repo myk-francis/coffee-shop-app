8a6dbe871388ca77e0a7a2ffabe53cdd09d0f297
myk <michaelmugendi1707@gmail.com>
2023-10-19 12:39:52 +0300

    Update: Bottom tab feature successfull add index files

---
+++ src/components/index.ts
@@ -0,0 +1,31 @@
+import BGIcon from './BGIcon';
+import GradientBGIcon from './GradientBGIcon';
+import CoffeeCard from './CoffeeCard';
+import FavoritesItemCard from './FavoritesItemCard';
+import CartItem from './CartItem';
+import HeaderBar from './HeaderBar';
+import OrderHistoryCard from './OrderHistoryCard';
+import ImageBackgroundImage from './ImageBackgroundImage';
+import PaymentFooter from './PaymentFooter';
+import PaymentMethod from './PaymentMethod';
+import ProfilePic from './ProfilePic';
+import EmptyListAnimation from './EmptyListAnimation';
+import OrderItemCard from './OrderItemCard';
+import PopUpAnimation from './PopUpAnimation';
+
+export {
+  BGIcon,
+  GradientBGIcon,
+  CoffeeCard,
+  FavoritesItemCard,
+  CartItem,
+  HeaderBar,
+  OrderHistoryCard,
+  ImageBackgroundImage,
+  PaymentFooter,
+  PaymentMethod,
+  ProfilePic,
+  EmptyListAnimation,
+  OrderItemCard,
+  PopUpAnimation,
+};
---
+++ src/screens/index.ts
@@ -0,0 +1,15 @@
+import CartScreen from './CartScreen';
+import FavoritesScreen from './FavoritesScreen';
+import PaymentScreen from './PaymentScreen';
+import OrderHistoryScreen from './OrderHistoryScreen';
+import HomeScreen from './HomeScreen';
+import DetailsScreen from './DetailsScreen';
+
+export {
+  CartScreen,
+  FavoritesScreen,
+  PaymentScreen,
+  OrderHistoryScreen,
+  HomeScreen,
+  DetailsScreen,
+};
