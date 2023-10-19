ed73f1f50891476d2d062fa47d75891d1e93a45e
myk <michaelmugendi1707@gmail.com>
2023-10-19 14:26:46 +0300

    Update: configure store and start home screen design

--- src/screens/HomeScreen.tsx
+++ src/screens/HomeScreen.tsx
@@ -1,7 +1,48 @@
 import {View, Text} from 'react-native';
 import React from 'react';
+import {useStore} from '../store/store';
+import {useBottomTabBarHeight} from '@react-navigation/bottom-tabs';
+
+const getCategoriesFromData = (data: any) => {
+  let temp: any = {};
+  for (let i = 0; i < data.length; i++) {
+    if (temp[data[i].name] === undefined) {
+      temp[data[i].name] = 1;
+    } else {
+      temp[data[i].name]++;
+    }
+  }
+  let categories = Object.keys(temp);
+  categories.unshift('All');
+  return categories;
+};
+
+const getCoffeeList = (category: string, data: any) => {
+  if (category === 'All') {
+    return data;
+  } else {
+    let coffeelist = data.filter((item: any) => item.name === category);
+    return coffeelist;
+  }
+};
 
 const HomeScreen = () => {
+  const CoffeeList = useStore((state: any) => state.CoffeeList);
+  const BeanList = useStore((state: any) => state.BeanList);
+  const [categories, setCategories] = React.useState(
+    getCategoriesFromData(CoffeeList),
+  );
+  const [searchText, setSearchText] = React.useState(undefined);
+  const [categoryIndex, setCategoryIndex] = React.useState({
+    index: 0,
+    category: categories[0],
+  });
+  const [sortedCoffee, setSortedCoffee] = React.useState(
+    getCoffeeList(categoryIndex.category, CoffeeList),
+  );
+
+  const tabBarHeight = useBottomTabBarHeight();
+
   return (
     <View>
       <Text>HomeScreen</Text>
