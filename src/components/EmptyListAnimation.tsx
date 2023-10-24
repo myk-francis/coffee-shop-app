8bfd33c01dd669dd6797f8003965ff49e93d1ec5
myk <michaelmugendi1707@gmail.com>
2023-10-24 09:16:16 +0300

    Update: cart screen completed

--- src/components/EmptyListAnimation.tsx
+++ src/components/EmptyListAnimation.tsx
@@ -1,14 +1,40 @@
-import {View, Text, StyleSheet} from 'react-native';
+import {StyleSheet, Text, View} from 'react-native';
 import React from 'react';
+import LottieView from 'lottie-react-native';
+import {COLORS, FONTFAMILY, FONTSIZE} from '../theme/theme';
 
-const EmptyListAnimation = () => {
+interface EmptyListAnimationProps {
+  title: string;
+}
+
+const EmptyListAnimation: React.FC<EmptyListAnimationProps> = ({title}) => {
   return (
-    <View>
-      <Text>EmptyListAnimation</Text>
+    <View style={styles.EmptyCartContainer}>
+      <LottieView
+        style={styles.LottieStyle}
+        source={require('../lottie/coffeecup.json')}
+        autoPlay
+        loop
+      />
+      <Text style={styles.LottieText}>{title}</Text>
     </View>
   );
 };
 
 export default EmptyListAnimation;
 
-const styles = StyleSheet.create({});
+const styles = StyleSheet.create({
+  EmptyCartContainer: {
+    flex: 1,
+    justifyContent: 'center',
+  },
+  LottieStyle: {
+    height: 300,
+  },
+  LottieText: {
+    fontFamily: FONTFAMILY.poppins_medium,
+    fontSize: FONTSIZE.size_16,
+    color: COLORS.primaryOrangeHex,
+    textAlign: 'center',
+  },
+});
